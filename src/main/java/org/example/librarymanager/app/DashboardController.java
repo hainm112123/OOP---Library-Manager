d41bc53825e358e11589e763a516a05334cb5378
hienduc14 <hiennguyenduc2k5@gmail.com>
2024-10-14 23:46:36 +0700

    test fx:include

--- src/main/java/org/example/librarymanager/app/DashboardController.java
+++ src/main/java/org/example/librarymanager/app/DashboardController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,18 +23,26 @@ public class DashboardController extends ControllerWrapper {
     }
 
     public void categoryButtonOnAction(ActionEvent event) {
-//        FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource(".fxml"));
-//        Scene scene = new Scene(fxmlLoader.load());
-//        switchScene(scene);
-//        stage.show();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource("document-detail.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            switchScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("categoryButtonOnAction");
     }
 
     public void profileButtonOnAction(ActionEvent event) {
-//        FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource(".fxml"));
-//        Scene scene = new Scene(fxmlLoader.load());
-//        switchScene(scene);
-//        stage.show();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource("profile.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            switchScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("profileButtonOnAction");
     }
 

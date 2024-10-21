b1f0cc451cd2faffb63f583097c07e4631d23235
yuuhi <hainm112123@gmail.com>
2024-10-21 15:58:45 +0700

    resolve conflict

--- src/main/java/org/example/librarymanager/app/TopbarController.java
+++ src/main/java/org/example/librarymanager/app/TopbarController.java
@@ -2,12 +2,19 @@ package org.example.librarymanager.app;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import org.example.librarymanager.Common;
+import org.example.librarymanager.data.CategoryQuery;
+import org.example.librarymanager.models.Category;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import static org.example.librarymanager.data.CategoryQuery.getCategoriesName;
 
 public class TopbarController extends ControllerWrapper {
     @FXML
@@ -23,17 +30,21 @@ public class TopbarController extends ControllerWrapper {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        topbarHomeBtn.setOnAction((event) -> safeSwitchScene("home.fxml"));
-        ObservableList<String> list = FXCollections.observableArrayList("Mathematics", "Technology", "Philosophy" );
+        topbarHomeBtn.setOnAction((event) -> switchScene("home.fxml"));
+        List<String> categoryList = CategoryQuery.getCategoriesName();
+        for (String categoryName : categoryList) {
+            System.out.println(categoryName);
+        }
+        ObservableList<String> list = FXCollections.observableArrayList(categoryList);
         topbarCategoryBtn.setItems(list);
         topbarCategoryBtn.setOnAction((event) -> {
             ControllerWrapper.setCurrentCategory(topbarCategoryBtn.getValue());
-            safeSwitchScene("category.fxml");
+            switchScene("category.fxml");
         });
 
-        topbarDocModifyBtn.setOnAction((event) -> safeSwitchScene("new-document.fxml"));
-        topbarProfileBtn.setOnAction((event) -> safeSwitchScene("profile.fxml"));
-        topbarMyDocBtn.setOnAction((event) -> safeSwitchScene("my-documents.fxml"));
+        topbarDocModifyBtn.setOnAction((event) -> switchScene("new-document.fxml"));
+        topbarProfileBtn.setOnAction((event) -> switchScene("profile.fxml"));
+        topbarMyDocBtn.setOnAction((event) -> switchScene("my-documents.fxml"));
     }
 
 }

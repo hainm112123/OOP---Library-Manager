93e53066ecf054b724e066c97e554eee6724895c
yuuhi <hainm112123@gmail.com>
2024-11-04 20:48:39 +0700

    quick fix

--- src/main/java/org/example/librarymanager/app/RegisterController.java
+++ src/main/java/org/example/librarymanager/app/RegisterController.java
@@ -1,8 +1,11 @@
 package org.example.librarymanager.app;
 
+import io.github.palexdev.materialfx.controls.MFXProgressSpinner;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import org.example.librarymanager.Common;
 import org.example.librarymanager.data.AuthQuery;
 import org.example.librarymanager.data.AuthResult;
 
@@ -10,17 +13,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RegisterController extends ControllerWrapper {
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
-
     @FXML
     private TextField firstname;
     @FXML
     private TextField lastname;
     @FXML
-    private TextField gender;
+    private ComboBox<String> gender;
     @FXML
     private DatePicker dateOfBirth;
     @FXML
@@ -33,10 +31,45 @@ public class RegisterController extends ControllerWrapper {
     private Label registerMessageLabel;
     @FXML
     private Hyperlink loginHyperlink;
+    @FXML
+    private MFXProgressSpinner loader;
+    @FXML
+    private Button submitBtn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gender.getItems().addAll("Male", "Female");
+        Common.disable(loader);
+    }
 
     public void registerButtonOnAction(ActionEvent event) {
-        AuthResult registerResult = AuthQuery.getInstance().register(username.getText(), password.getText(), confirmPassword.getText(), firstname.getText(), lastname.getText(), gender.getText(), dateOfBirth.getValue());
-        registerMessageLabel.setText(registerResult.getMessage());
+        Task<AuthResult> task = new Task<AuthResult>() {
+            @Override
+            protected AuthResult call() throws Exception {
+                return AuthQuery.getInstance().register(
+                        username.getText(),
+                        password.getText(),
+                        confirmPassword.getText(),
+                        firstname.getText(),
+                        lastname.getText(),
+                        gender.getValue(),
+                        dateOfBirth.getValue()
+                );
+            }
+        };
+        Common.disable(submitBtn);
+        Common.enable(loader);
+        task.setOnSucceeded((e) -> {
+            Common.disable(loader);
+            Common.enable(submitBtn);
+            AuthResult authResult = task.getValue();
+            registerMessageLabel.setText(authResult.getMessage());
+            if (authResult.getUser() != null) {
+                safeSwitchScene("login.fxml");
+                stage.show();
+            }
+        });
+        new Thread(task).start();
     }
 
     public void loginButtonOnAction(ActionEvent event) {
